40cd8abb6d0bd337257f964ca82df64cddc70ae1
osvanicolina <osvacolijim@gmail.com>
2019-11-03 12:30:17 -0400

    add axios requests to get information from direction

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const { argv } = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +8,14 @@ const { argv } = require('yargs').options({
     }
 });
 
-console.log(argv.direccion);
+const encodeUrl = encodeURI(argv.direccion);
+console.log(encodeUrl);
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+    headers: { 'x-rapidapi-key': '422e04f3d3mshc633a01181b4161p172596jsn3ad21bc83ef9' }
+});
+
+
+instance.get()
+    .then(resp => console.log(resp.data.Results[0]))
+    .catch(err => console.log('ERROR!!!!!', err));
